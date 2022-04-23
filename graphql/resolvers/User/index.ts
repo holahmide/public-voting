49b1fb4c3e15260a0199982cc7b9eddc4867f3c4
Adeniyi Olamide <olami02bj@gmail.com>
2022-04-23 21:06:40 +0100

    Progress Push

--- graphql/resolvers/User/index.ts
+++ graphql/resolvers/User/index.ts
@@ -5,10 +5,6 @@ export default {
   User: {
     createdAt: (parent: any) => parent.created_at.toString(),
   },
-  Token: {
-    expires: (parent: any) => parent.expires.toString(),
-    createdAt: (parent: any) => parent.created_at.toString(),
-  },
   Query: {
     user: async (_: any, __: any, context: GraphqlContext) => {
       if (context.isLoggedIn) {
@@ -16,14 +12,14 @@ export default {
       }
       return new AuthenticationError('you are not logged in');
     },
-    users: async (_: any, args: any, context: GraphqlContext) => {
-      if (context.isSuperUser) {
-        return {
-          data: User.find({}),
-        };
-      }
-      return new AuthenticationError('you are not a super user');
-    },
+    // users: async (_: any, args: any, context: GraphqlContext) => {
+    //   if (context.isSuperUser) {
+    //     return {
+    //       data: User.find({}),
+    //     };
+    //   }
+    //   return new AuthenticationError('you are not a super user');
+    // },
     userById: async (_: any, args: any, context: GraphqlContext) => {
       const user = await User.findById(args.id).populate('roles');
       if (user) return user;
--- graphql/schemas/User/index.ts
+++ graphql/schemas/User/index.ts
@@ -8,14 +8,14 @@ export default gql`
     regNo: Int
     email: String
     isConfirmed: Boolean
-    roles: [Role]
+    # roles: [Role]
     createdAt: String
     updatedAt: String
   }
   
   extend type Query {
     user: User
-    users(skip: Int, limit: Int, keyword: String): UserSearch
+    # users(skip: Int, limit: Int, keyword: String): UserSearch
     userById(id: String!): User
   }
 `;
